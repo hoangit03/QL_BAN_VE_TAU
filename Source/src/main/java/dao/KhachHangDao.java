9de3a2ffd1dd5cf44ea2523fd3a69668299e3ce1
tan1504 <pmt150403@gmail.com>
2024-04-15 16:14:06 +0700

    add KhachHangDao

--- Source/src/main/java/dao/KhachHangDao.java
+++ Source/src/main/java/dao/KhachHangDao.java
@@ -1,14 +1,42 @@
 
 package dao;
 
-import com.sun.jdi.connect.spi.Connection;
 import entity.KhachHang;
-import java.beans.Statement;
-import java.util.ArrayList;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+
 import java.util.List;
 
 public class KhachHangDao {
+    private EntityManager em;
     
+    public KhachHangDao(EntityManagerFactory emf) {
+		em = emf.createEntityManager();
+	}
+    
+    // Update KhachHang
+    public boolean updateKhachHang(KhachHang khachHang) {
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
+			em.merge(khachHang);
+			tx.commit();
+			return true;
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		}
+		return false;
+	}
+    
+	//  Get list KhachHang
+	public List<KhachHang> getAllKhachHang() {
+		try {
 
-    
+			return em.createNamedQuery("KhachHang.findAll", KhachHang.class).getResultList();
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
